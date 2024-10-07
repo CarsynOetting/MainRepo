41b69b4db9ea99be15067fbad2b05fabe3776600
coetting <carsonoetting@gmail.com>
2024-10-07 17:09:14 -0600

    updated main script with imports

--- Projects/Main.py
+++ Projects/Main.py
@@ -1,1 +1,9 @@
-# Tracking for that one file in COursera
+# Tracking for that one project in Coursera
+
+
+#--IMPORT SECTION--
+import urllib.request, urllib.parse, urllib.error
+import http
+import sqlite3
+
+
