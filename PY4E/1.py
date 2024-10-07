4bbcd1480600b93e44246c2fcfc24d85ff8947a2
coetting <carsonoetting@gmail.com>
2024-10-07 14:30:49 -0600

    Updated files

---
+++ PY4E/1.py
@@ -0,0 +1,22 @@
+# #Testing classes
+
+# class TVA_EMP:
+#     def __init__(self):
+#         self.x = 0
+
+#     def common(self):
+#         self.x = self.x + 1
+#         print ("Commonality is",self.x)
+            
+# AR = TVA_EMP()
+
+# AR.common()
+# AR.common()
+# AR.common()
+
+
+x = -1
+for value in [3, 41, 12, 9, 74, 15] :
+    if value > x :
+        x = value
+print(x)
