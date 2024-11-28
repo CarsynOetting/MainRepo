9591e732d8c8feb20e6cb0c6c8cd23bbee711306
coetting <carsonoetting@gmail.com>
2024-11-28 10:31:01 -0700

    Added parser for grandma tablet contacts

---
+++ MISC/contacts parser.py
@@ -0,0 +1,21 @@
+file = open("C:/Users/carso/Documents/Cam_contacts.txt")
+
+name = ""
+phone_num = ""
+
+for line in file:
+    if not line.strip():  # Check if the line is empty
+        continue  # Skip to the next iteration
+    if line[0] == " ":
+        continue
+    line_break = line.split(':')
+    line_1 = line_break[0]
+    line_2 = line_break[1]
+
+
+    if line_1 == "FN":
+        name = line_2
+    if line_1 == "TEL;CELL":
+        phone_num = line_2
+        finished_line = name + " : " + phone_num
+        print(finished_line) 
